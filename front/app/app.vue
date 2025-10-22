4354ea873ad73ad24ae512bafb1f6161c8878414
DMS深尾 <t.fukao@dm-s.co.jp>
2025-10-22 15:16:35 +0900

    タイトルを動的にする

--- front/app/app.vue
+++ front/app/app.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const APP_NAME = runtimeConfig.public.appName;
+useHead({
+  titleTemplate: (titleChunk?: string): string => {
+    const title = titleChunk ? `${titleChunk} | ${APP_NAME}` : APP_NAME;
+    return title;
+  },
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
