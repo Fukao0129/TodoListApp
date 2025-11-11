51fcf5dc5056c27e9f5ebc103aed806f3351b985
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:48:36 +0900

    デフォルトのレイアウトファイルを作成

---
+++ front/app/layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts" />
+
+<template>
+  <TheHeader />
+
+  <div class="main__wrapper">
+    <TheSidebar />
+    <main class="main__content">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.main__wrapper {
+  display: flex;
+  min-height: 100vh;
+}
+.main__content {
+  width: 100%;
+  background-color: var(--background-color);
+  margin-left: 10rem; /* Sidebarの分 */
+  margin-top: 3.5rem; /* Headerの分 */
+  padding: 2rem;
+}
+</style>
