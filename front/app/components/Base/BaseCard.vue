f5f18a783a4823c1059ff6fcbc2d07cce6f56461
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:46:05 +0900

    カードコンポーネントを作成

---
+++ front/app/components/Base/BaseCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+export type BaseModalProps = {
+  backgroundColor?: "white" | "lightgray";
+};
+withDefaults(defineProps<BaseModalProps>(), {
+  backgroundColor: "white",
+});
+</script>
+
+<template>
+  <div class="card" :class="`card--${backgroundColor}`">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid var(--border-color);
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  width: 100%;
+}
+
+/** 種類によって背景色を変える */
+.card--white {
+  background: white;
+}
+.card--lightgray {
+  background: var(--disabled-color);
+}
+</style>
