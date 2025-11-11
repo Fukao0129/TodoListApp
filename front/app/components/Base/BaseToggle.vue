59dc1fbb525cce4c86da7c1544eba71c05a5a73b
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:29:07 +0900

    トグルコンポーネントを作成

---
+++ front/app/components/Base/BaseToggle.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+export type BaseToggleProps = {
+  label?: string;
+};
+defineProps<BaseToggleProps>();
+
+const isOn = defineModel<boolean>("isOn");
+
+/** ON/OFF切り替え */
+const onToggle = () => {
+  isOn.value = !isOn.value;
+};
+</script>
+
+<template>
+  <div class="toggle__wrapper">
+    <span class="toggle__label">{{ label }}</span>
+    <div
+      class="toggle__button"
+      :class="{ 'is-on': isOn }"
+      tabindex="0"
+      @click="onToggle"
+      @keydown.enter="onToggle"
+    >
+      <div class="toggle__button--circle"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.toggle__wrapper {
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  .toggle__label {
+    font-size: 0.8rem;
+  }
+
+  .toggle__button {
+    width: 2.2rem;
+    height: 1.2rem;
+    background-color: var(--border-color);
+    border-radius: 15px;
+    position: relative;
+    cursor: pointer;
+    transition: background-color 0.2s;
+    .toggle__button--circle {
+      width: 1rem;
+      height: 1rem;
+      background-color: #fff;
+      border-radius: 50%;
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      transition: left 0.2s;
+    }
+  }
+
+  .toggle__button.is-on {
+    background-color: var(--primary-color);
+  }
+  .toggle__button.is-on .toggle__button--circle {
+    left: calc(100% - 1rem - 2px);
+  }
+}
+</style>
