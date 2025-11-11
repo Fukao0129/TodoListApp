cae028f692c58ee7cfd6f97ff2b1383c0d7aa6fc
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-11 12:58:08 +0900

    Todoの型定義を作成

---
+++ front/app/types/todo.ts
@@ -0,0 +1,31 @@
+// 共通構造
+type BaseTodo = {
+  title: string;
+  description: string | null;
+  user_id: number;
+  status_id: number;
+  due_date: string | null;
+  priority: number;
+  reminder_at: string | null;
+};
+
+// GETレスポンス
+export type Todo = BaseTodo & {
+  id: number;
+  status: {
+    id: number;
+    name: string;
+  };
+  completed_at: string | null;
+  reminder_at: string | null;
+  created_at?: string;
+  updated_at?: string;
+};
+
+// POSTリクエスト
+export type CreateTodoRequest = BaseTodo;
+
+// PUTリクエスト
+export type UpdateTodoRequest = BaseTodo & {
+  id: number;
+};
