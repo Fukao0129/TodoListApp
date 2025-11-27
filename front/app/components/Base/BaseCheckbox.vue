7fbcf24e687588f11d0f9df34d6e1a783e767783
DMS深尾 <t.fukao@dm-s.co.jp>
2025-11-27 12:40:54 +0900

    チェックボックスコンポーネントを作成

---
+++ front/app/components/Base/BaseCheckbox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const isChecked = defineModel<boolean>("isChecked");
+
+const emit = defineEmits<{
+  onCheck: [boolean];
+}>();
+</script>
+
+<template>
+  <input
+    type="checkbox"
+    :checked="isChecked"
+    @click="emit('onCheck', ($event.target as HTMLInputElement)?.checked)"
+    @keydown.enter="
+      emit('onCheck', !($event.target as HTMLInputElement)?.checked)
+    "
+  />
+</template>
+
+<style scoped>
+input {
+  accent-color: var(--primary-color);
+  cursor: pointer;
+}
+</style>
