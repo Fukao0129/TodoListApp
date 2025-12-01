d2f9f6f594f3160148fae498c898eee54ba18836
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-01 11:10:04 +0900

    TODOのモデルクラスを作成

---
+++ api/app/Models/Todo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'status_id',
+        'due_date',
+        'priority',
+        'is_trashed',
+        'completed_at',
+        'reminder_at',
+    ];
+
+    public function status()
+    {
+        return $this->belongsTo('App\Models\Status');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
