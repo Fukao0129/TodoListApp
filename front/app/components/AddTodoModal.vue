6a54c438dd137d05564bf6c2362aea3a6a832bc0
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-01 10:18:15 +0900

    TODO追加モーダルを作成

---
+++ front/app/components/AddTodoModal.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import type { CreateTodoRequest } from "@/types/todo";
+
+const isShow = defineModel<boolean>("isShow");
+
+const emit = defineEmits<{
+  submit: [typeof formData.value];
+}>();
+
+const { validationErrors, clearErrorMessages } = useValidationErrors();
+
+// フォーム
+const formData = computed<CreateTodoRequest>(() => ({
+  title: "",
+  description: "",
+  priority: PRIORITY.LOW.value,
+  user_id: 1,
+  status_id: DEFAULT_STATUSES.NOT_STARTED.value,
+  due_date: "",
+  reminder_at: "",
+}));
+
+/** 追加ボタン押下 */
+const onClickSubmit = () => {
+  emit("submit", formData.value);
+};
+
+/** タイトルの入力欄にフォーカスする */
+watch(isShow, (newVal) => {
+  clearErrorMessages();
+  if (newVal) {
+    nextTick(() => {
+      focusOnElement(".add-todo__title-wrapper input");
+    });
+  }
+});
+</script>
+
+<template>
+  <BaseModal v-model:is-show="isShow" title="Todoを追加する">
+    <template #content>
+      <form @submit.prevent="onClickSubmit">
+        <FormItem label="タイトル">
+          <div class="add-todo__title-wrapper">
+            <BaseInput
+              v-model:text="formData.title"
+              :error-message="validationErrors['add-todo.title']"
+            />
+          </div>
+        </FormItem>
+        <FormItem label="説明">
+          <BaseTextarea v-model:text="formData.description" />
+        </FormItem>
+        <FormItem label="優先度">
+          <BaseSelect
+            v-model:selected-value="formData.priority"
+            :options="
+              Object.values(PRIORITY).map((priority) => ({
+                id: priority.value,
+                name: priority.label,
+              }))
+            "
+          />
+        </FormItem>
+        <FormItem label="リマインド">
+          <input
+            v-model="formData.reminder_at"
+            type="datetime-local"
+            name="reminder_at"
+          />
+        </FormItem>
+        <FormItem label="期日" :has-border="false">
+          <input
+            v-model="formData.due_date"
+            type="datetime-local"
+            name="due_date"
+          />
+        </FormItem>
+      </form>
+    </template>
+    <template #footer>
+      <BaseButton text="キャンセル" type="secondary" @click="isShow = false" />
+      <BaseButton text="追加" @click="onClickSubmit" />
+    </template>
+  </BaseModal>
+</template>
+
+<style scoped>
+:deep(.modal__wrapper) {
+  width: 50%;
+}
+</style>
