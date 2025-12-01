06b502f5e51b1438db11fb3119683049190ccc9d
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-01 10:51:36 +0900

    優先度に対応する色を定数で持っておく

--- front/app/constants/priority.ts
+++ front/app/constants/priority.ts
@@ -3,13 +3,16 @@ export const PRIORITY = {
   LOW: {
     value: 1,
     label: "低",
+    color: "info" as const,
   },
   MEDIUM: {
     value: 2,
     label: "中",
+    color: "warning" as const,
   },
   HIGH: {
     value: 3,
     label: "高",
+    color: "error" as const,
   },
 };
