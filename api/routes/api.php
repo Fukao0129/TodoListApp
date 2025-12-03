dfd3870603f4fded6ddbdd30f0fed8e70ecdf482
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-03 14:08:22 +0900

    APIのルーティングを設定

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,22 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Todo
-Route::get('/todos', 'App\Http\Controllers\TodoController@index');
-Route::get('/todos/{todo_id}', 'App\Http\Controllers\TodoController@show');
-Route::put('/todos/{todo_id}', 'App\Http\Controllers\TodoController@update');
-Route::post('/todos', 'App\Http\Controllers\TodoController@store');
-Route::delete('/todos/{todo_id}', 'App\Http\Controllers\TodoController@delete');
-Route::delete('/todos', 'App\Http\Controllers\TodoController@bulkDelete');
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-// ステータス
-Route::get('/statuses', 'App\Http\Controllers\StatusController@index');
-Route::post('/statuses', 'App\Http\Controllers\StatusController@store');
-Route::put('/statuses/{status_id}', 'App\Http\Controllers\StatusController@update');
-Route::delete('/statuses/{status_id}', 'App\Http\Controllers\StatusController@delete');
+    // ユーザー情報
+    Route::get('/me', 'App\Http\Controllers\UserController@me')->name('me');
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    // Todo
+    Route::get('/todos', 'App\Http\Controllers\TodoController@index');
+    Route::get('/todos/{todo_id}', 'App\Http\Controllers\TodoController@show');
+    Route::put('/todos/trash-completed', 'App\Http\Controllers\TodoController@trashAllCompleted');
+    Route::put('/todos/{todo_id}', 'App\Http\Controllers\TodoController@update');
+    Route::post('/todos', 'App\Http\Controllers\TodoController@store');
+    Route::delete('/todos/{todo_id}', 'App\Http\Controllers\TodoController@delete');
+    Route::delete('/todos', 'App\Http\Controllers\TodoController@bulkDelete');
+
+    // ステータス
+    Route::get('/statuses', 'App\Http\Controllers\StatusController@index');
+    Route::post('/statuses', 'App\Http\Controllers\StatusController@store');
+    Route::put('/statuses/{status_id}', 'App\Http\Controllers\StatusController@update');
+    Route::delete('/statuses/{status_id}', 'App\Http\Controllers\StatusController@delete');
+});
