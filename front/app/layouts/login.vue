d0fbc77ddbf74eeda71033e2872332dfebcf5d53
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-08 10:38:52 +0900

    ログイン画面のレイアウトをレイアウトファイルに移動

--- front/app/layouts/login.vue
+++ front/app/layouts/login.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts" />
 
 <template>
-  <slot />
+  <div class="login-page__wrapper">
+    <BaseCard class="login-page__content">
+      <img
+        src="/assets/img/logo-color.svg"
+        alt="TODO"
+        class="login-page__logo"
+      />
+      <slot />
+    </BaseCard>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.login-page__wrapper {
+  height: 100vh;
+  background: var(--background-color);
+  display: flex;
+  justify-content: center;
+  .login-page__content {
+    max-width: 30rem;
+    margin: auto;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    text-align: center;
+    .login-page__logo {
+      max-width: 10rem;
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
