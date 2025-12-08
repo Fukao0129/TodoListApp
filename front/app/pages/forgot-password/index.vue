710e0a5819ad4c417ddc22bde7a6b34f26e3b3af
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-08 10:37:29 +0900

    パスワードリセットのメール送信、再設定ページを作成

---
+++ front/app/pages/forgot-password/index.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+definePageMeta({
+  sanctum: { excluded: true },
+});
+useHead({ title: "パスワードリセットメールの送信" });
+
+const { callApi } = useApi();
+const { showSnackbar } = useSnackbar();
+
+/** パスワードリセットメール送信 */
+const email = ref<string>("");
+const onSubmitPasswordReset = () => {
+  callApi(`/forgot-password`, {
+    method: "POST",
+    body: { email: email.value },
+  })
+    .then(async () => {
+      showSnackbar("パスワードリセットメールを送信しました");
+    })
+    .catch(() => {
+      showSnackbar("パスワードリセットメールの送信に失敗しました。", "error");
+    });
+};
+</script>
+
+<template>
+  <NuxtLayout name="login">
+    <form @submit.prevent="onSubmitPasswordReset()">
+      <FormItem :has-border="false">
+        <BaseInput
+          v-model:text="email"
+          label="メールアドレス"
+          placeholder="メールアドレスを入力してください"
+        />
+      </FormItem>
+
+      <NuxtLink to="/login" class="back-login__link"
+        >← ログイン画面に戻る</NuxtLink
+      >
+
+      <BaseButton text="パスワードリセットメールを送信する" />
+    </form>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+.back-login__link {
+  font-size: 0.8rem;
+  color: var(--primary-color);
+  margin: 0.5rem 0 1rem;
+  display: block;
+}
+</style>
---
+++ front/app/pages/forgot-password/reset.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+definePageMeta({
+  sanctum: { excluded: true },
+});
+useHead({ title: "パスワードリセット" });
+
+const route = useRoute();
+const { callApi } = useApi();
+const { validationErrors, setErrorMessages, clearErrorMessages } =
+  useValidationErrors();
+
+// パスワードリセットフォーム
+const { token, email } = route.query;
+const passwordFormData = ref({
+  token: token || "",
+  email: email || "",
+  password: "",
+  password_confirmation: "",
+});
+
+/** パスワードリセット */
+const onResetPassword = () => {
+  clearErrorMessages();
+  callApi(`/password/reset`, {
+    method: "POST",
+    body: passwordFormData.value,
+  })
+    .then(() => {
+      navigateTo("/login");
+    })
+    .catch((error) => {
+      setErrorMessages(error.data.errorMessage, "reset-password");
+    });
+};
+</script>
+<template>
+  <NuxtLayout name="login">
+    <form class="user-edit__wrapper" @submit.prevent="onResetPassword()">
+      <FormItem :has-border="false">
+        <BaseInput
+          v-model:text="passwordFormData.password"
+          placeholder="パスワードを入力してください"
+          label="パスワード"
+          :error-message="validationErrors['reset-password.password']"
+        />
+      </FormItem>
+      <FormItem :has-border="false">
+        <BaseInput
+          v-model:text="passwordFormData.password_confirmation"
+          placeholder="パスワードを入力してください"
+          label="パスワード(確認)"
+          :error-message="
+            validationErrors['reset-password.password_confirmation']
+          "
+        />
+      </FormItem>
+      <BaseButton
+        text="パスワードをリセットする"
+        class="password-reset__button"
+      />
+    </form>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+.password-reset__button {
+  margin-top: 1rem;
+}
+</style>
