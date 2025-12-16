6f52df1ff7d419f37de998efcb9e2d7c19eb76e6
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-16 12:42:51 +0900

    シーダー実行時にでテストユーザーを固定で生成する

--- api/database/seeders/UsersTableSeeder.php
+++ api/database/seeders/UsersTableSeeder.php
@@ -3,12 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
     public function run()
     {
+        User::factory()->create([
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
         User::factory()->count(10)->create();
     }
 }
