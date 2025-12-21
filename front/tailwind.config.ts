ccdab235c4a65c47c9f8c8d9fa5b234c4ece7e01
DMS深尾 <t.fukao@dm-s.co.jp>
2025-12-21 22:38:24 +0900

    Tailwindの設定ファイルを追加

---
+++ front/tailwind.config.ts
@@ -0,0 +1,46 @@
+// TailwindCSSの拡張設定 https://tailwindcss.nuxtjs.org/tailwindcss/configuration
+import colors from "tailwindcss/colors";
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#007e70",
+          strong: "#005f57",
+          muted: "#55beb6",
+          subtle: "#e0f7f5",
+        },
+        neutral: {
+          DEFAULT: "#333333",
+          strong: "#000000",
+          muted: "#666666",
+          subtle: "#dfdfdf",
+        },
+        info: {
+          DEFAULT: colors.blue[500],
+          strong: colors.blue[700],
+          muted: colors.blue[300],
+          subtle: colors.blue[100],
+        },
+        warning: {
+          DEFAULT: colors.amber[500],
+          strong: colors.amber[700],
+          muted: colors.amber[300],
+          subtle: colors.amber[100],
+        },
+        error: {
+          DEFAULT: colors.red[500],
+          strong: colors.red[700],
+          muted: colors.red[300],
+          subtle: colors.red[100],
+        },
+      },
+      spacing: {
+        "header-height": "3.5rem",
+        "sidebar-width": "10rem",
+      },
+    },
+  },
+  plugins: [],
+};
